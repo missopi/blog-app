89e7d169175480856a649d3d0a5fd6e430fb2f7f
missopi <missopi@me.com>
2024-01-27 14:46:27 +0000

    Tidy up article link in index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@
 Rails.application.routes.draw do
   root 'articles#index'
 
-  get 'up' => 'rails/health#show', as: :rails_health_check
-  get '/articles', to: 'articles#index'
-  get '/articles/:id', to: 'articles#show'
+  resources :articles
 end
