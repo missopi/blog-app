04b5bcc83b7bac7a9362cd18c9570cee7c09acb3
missopi <missopi@me.com>
2024-01-27 14:56:30 +0000

    Add validations to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# article class
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
